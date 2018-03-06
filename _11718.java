0409fc355b1ae0311fa04f136368684c64ae154d
iceCreamParlor <heejjeeh@gmail.com>
2018-03-06 20:43:38 +0900

    mar06

---
+++ _11718.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class _11718
+{
+	public static void main(String[] args) throws FileNotFoundException{
+		Scanner sc = new Scanner(System.in);
+		while( true ){
+			try{
+				String str = sc.nextLine();
+				System.out.println(str);
+			}catch(Exception e){
+				break;
+			}
+		}
+	}
+}
